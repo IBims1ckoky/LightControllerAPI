b607f35ea2c8d07ae129f3056394193c47a4b701
IBims1ckoky <business.maxi.zink@gmail.com>
2022-02-12 18:33:47 +0100

    added discovery for rooms

--- api/src/main/java/de/maxizink/lightcontroller/discovery/room/service/HueRoomDiscoveryService.java
+++ api/src/main/java/de/maxizink/lightcontroller/discovery/room/service/HueRoomDiscoveryService.java
@@ -1,14 +1,19 @@
 package de.maxizink.lightcontroller.discovery.room.service;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import de.maxizink.lightcontroller.discovery.bridge.api.HueBridge;
 import de.maxizink.lightcontroller.discovery.room.api.HueRoom;
 import de.maxizink.lightcontroller.discovery.room.api.HueRoomDiscovery;
+import de.maxizink.lightcontroller.discovery.room.exception.HueRoomDiscoveryException;
+import de.maxizink.lightcontroller.discovery.room.model.HueRoomImpl;
 import de.maxizink.lightcontroller.utils.HueResponseValidator;
 import de.maxizink.lightcontroller.utils.JsonUtils;
 import de.maxizink.lightcontroller.utils.URLFormatter;
 import org.apache.http.client.methods.HttpUriRequest;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static de.maxizink.lightcontroller.utils.HttpUtils.createRequest;
@@ -21,9 +26,16 @@ public class HueRoomDiscoveryService implements HueRoomDiscovery {
     HttpUriRequest httpRequest = createRequest(URLFormatter.getAllRooms(hueBridge.getHueBridgeCredentials().getIpAddress()));
     JsonObject dataObject = JsonUtils.fromJson(executeJson(httpRequest));
     if (HueResponseValidator.hasErrors(dataObject)) {
-
+      throw new HueRoomDiscoveryException("Error while discover Rooms");
     }
 
-    return List.of();
+    List<HueRoom> hueRooms = new ArrayList<>();
+    JsonArray jsonLampArray = dataObject.getAsJsonArray("data");
+    for (JsonElement jsonElement : jsonLampArray) {
+      HueRoom hueRoom = new HueRoomImpl(jsonElement.getAsJsonObject(), hueBridge);
+      hueRooms.add(hueRoom);
+    }
+
+    return hueRooms;
   }
 }
