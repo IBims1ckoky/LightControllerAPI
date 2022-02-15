1216bc4c6cfa03f7ea22a7e3cf6f2d79d783e8fa
IBims1ckoky <business.maxi.zink@gmail.com>
2022-02-15 20:31:31 +0100

    added credentials for deployment

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -98,14 +98,13 @@ publishing {
                 name = "oss"
                 val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
                 val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-                url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl;
-                credentials.username = sonatypeUsername
-                credentials.password = sonatypePassword
 
                 credentials {
                     username = sonatypeUsername
                     password = sonatypePassword
                 }
+
+                url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl;
             }
         }
     }
